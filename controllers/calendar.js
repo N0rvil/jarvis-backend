e3d98bc5d16c7fc990dc26aaa8d70df9db16c4de
Filip Duda <dudafilip80@seznam.cz>
2021-04-19 09:06:35 +0200

    fixing calendar date bug

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -83,7 +83,7 @@ const daysWithEvents = (events, blockedEvents) => {
         const date = new Date(event.date);
         let year = date.getFullYear();
         let month = date.getMonth()+1;
-        let day = date.getDate(); //Before pushing into production add +1 because server have different date propably // let day = date.getDate()+1;
+        let day = date.getDate()+1; //Before pushing into production add +1 because server have different date propably // let day = date.getDate()+1;
 
 
         if (event.repeat === 'norepeat') {
